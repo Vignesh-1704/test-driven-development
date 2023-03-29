c4ac60295ab567130bccd2ba48809d5cbc37874d
Vignesh-1704 <vigneshmanikavasagam17@gmail.com>
2023-03-29 12:22:06 +0530

    Initial Commit Done

---
+++ src/test/java/com.m2p/ArithmeticOperationsTest.java
@@ -0,0 +1,7 @@
+package com.m2p;
+
+class ArithmeticOperationsTest{
+    public static void main(String[] args) {
+        ArithmeticOperations arithmeticOperations = new ArithmeticOperations();
+    }
+}
